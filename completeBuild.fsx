060478ecdf060cdc17d32656b19d3ad3baa0b541
p.kanavos <p.kanavos@pamediakopes.gr>
2016-11-22 20:06:07 +0200

    Changes to the full example

--- completeBuild.fsx
+++ completeBuild.fsx
@@ -1,10 +1,15 @@
 // include Fake lib
 #r @"packages\FAKE\tools\FakeLib.dll"
 
+//That's for Powershell
+#r "System.Management.Automation"
+
 open Fake
 open Fake.AssemblyInfoFile
 
+
 RestorePackages()
+
 
 // Directories
 let buildDir  = @".\build\"
@@ -17,6 +22,31 @@ let fxCopRoot = @".\Tools\FxCop\FxCopCmd.exe"
 
 // version info
 let version = "0.2"  // or retrieve from CI server
+
+// That's my version function! Year.WeekNumber.DayOfWeek.Hour
+let myVersion =
+     let dfi=System.Globalization.DateTimeFormatInfo.CurrentInfo
+     let calendar=dfi.Calendar
+
+     let now=DateTime.Now
+     let weekNum=calendar.GetWeekOfYear(now,dfi.CalendarWeekRule,dfi.FirstDayOfWeek)
+     String.Format("{0:yy}.{1}.{2}.{0:HHmm}",now,weekNum,(int)now.DayOfWeek  )
+
+  let InvokeRemote server command =
+    let block = ScriptBlock.Create(command)
+    let pipe=PowerShell.Create()    
+              .AddCommand("invoke-command")
+    pipe.AddParameter("ComputerName", server)            
+        .AddParameter("ScriptBlock", block)
+        .Invoke() 
+        |> Seq.map (sprintf  "%O")
+        |> Seq.iter (fun line ->
+                            let tracer=if line.Contains("not installed") then
+                                            traceError 
+                                       else 
+                                            trace
+                            tracer line)
+    pipe.Streams.Error |> Seq.iter (traceError << sprintf "%O" )
 
 // Targets
 Target "Clean" (fun _ ->
